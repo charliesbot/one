5598f8cf0dac72a0390df59c20665c56e0a0db30
Charlie <charliesbot@gmail.com>
2026-06-01 21:01:16 -0700

    chore(core): extract domain module (#58)

---
+++ core/domain/build.gradle.kts
@@ -0,0 +1,16 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins { alias(libs.plugins.kotlin.jvm) }
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin { compilerOptions { jvmTarget.set(JvmTarget.JVM_11) } }
+
+dependencies {
+  api(project(":core:model"))
+  api(platform(libs.kotlinx.coroutines.bom))
+  api(libs.kotlinx.coroutines.core)
+}
