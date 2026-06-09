4363efb970d2a0dfcec475b2031fd7047ab994cf
Charlie <charliesbot@gmail.com>
2026-06-08 19:32:09 -0700

    fix(widget): pin wear widget sample dependencies (#100)

--- widget/wear/src/main/java/com/charliesbot/one/widget/wear/OneWearWidget.kt
+++ widget/wear/src/main/java/com/charliesbot/one/widget/wear/OneWearWidget.kt
@@ -2,7 +2,7 @@ package com.charliesbot.one.widget.wear
 
 import android.content.Context
 import androidx.compose.remote.creation.compose.layout.RemoteComposable
-import androidx.compose.remote.creation.compose.layout.RemoteText
+import androidx.compose.remote.creation.compose.state.RemoteString
 import androidx.compose.runtime.Composable
 import androidx.glance.wear.GlanceWearWidget
 import androidx.glance.wear.WearWidgetBrush
@@ -10,6 +10,7 @@ import androidx.glance.wear.WearWidgetData
 import androidx.glance.wear.WearWidgetDocument
 import androidx.glance.wear.core.WearWidgetParams
 import androidx.wear.compose.remote.material3.RemoteMaterialTheme
+import androidx.wear.compose.remote.material3.RemoteText
 
 class OneWearWidget : GlanceWearWidget() {
   override suspend fun provideWidgetData(
@@ -24,5 +25,7 @@ class OneWearWidget : GlanceWearWidget() {
 @RemoteComposable
 @Composable
 private fun OneWearWidgetContent() {
-  RemoteMaterialTheme { RemoteText("ONE") }
+  RemoteMaterialTheme {
+    RemoteText(text = RemoteString("ONE"))
+  }
 }
