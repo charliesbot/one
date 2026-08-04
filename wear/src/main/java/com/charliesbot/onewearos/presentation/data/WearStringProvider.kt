47e583b21ff5e992c1dad0e14058ee85f8b042a6
Charlie L <charliesbot@gmail.com>
2026-08-03 20:18:54 -0700

    fix(resources): retain release strings

--- wear/src/main/java/com/charliesbot/onewearos/presentation/data/WearStringProvider.kt
+++ wear/src/main/java/com/charliesbot/onewearos/presentation/data/WearStringProvider.kt
@@ -1,11 +1,10 @@
 package com.charliesbot.onewearos.presentation.data
 
 import android.content.Context
+import com.charliesbot.shared.core.domain.platform.StringKey
 import com.charliesbot.shared.core.domain.platform.StringProvider
+import com.charliesbot.shared.core.platform.getString
 
 class WearStringProvider(private val context: Context) : StringProvider {
-  override fun getString(resourceId: String): String {
-    val resourceIdInt = context.resources.getIdentifier(resourceId, "string", context.packageName)
-    return context.getString(resourceIdInt)
-  }
+  override fun getString(key: StringKey): String = context.getString(key)
 }
